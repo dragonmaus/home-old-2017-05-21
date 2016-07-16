3687f3a6994d67281233c9f7e59f28a0c2e98740
David Wildasin <swdragonmaus@gmail.com>
2016-07-16 17:33:30 -0400

    Refactor Ruby libs

--- lib/ruby/canon.rb
+++ lib/ruby/canon.rb
@@ -5,82 +5,89 @@ require 'net/http'
 module Addressable
   class URI
     def canonical
-      uri = fixup
+      dup.canonical!
+    end
 
-      response = uri.fetch
-      response.uri = uri if response.uri.nil?
+    def canonical!
+      fixup!
+
+      response = fetch
+      response.uri ||= dup
       response.uri.normalize!
 
-      unless response.is_a?(Net::HTTPOK)
+      unless response.is_a? Net::HTTPOK
         warn "#{response.uri} returned status code #{response.code}"
-        return response.uri
+        return replace_self(response.uri)
       end
 
-      return response.uri unless response.content_type == 'text/html'
+      return replace_self(response.uri) unless response.content_type == 'text/html'
 
       document = Hpricot.parse(response.body)
       # <link rel="canonical"> takes priority over <meta property="og:url">
-      uri = if (element = document.at('link[@rel="canonical"]'))
-              Addressable::URI.parse(element['href'])
-            elsif (element = document.search('meta[@property$="url"]')
-                                     .select { |e| e['property'] == 'og:url' }
-                                     .first)
-              Addressable::URI.parse(element['content'])
-            else
-              response.uri
-            end
+      replace_self(
+        if (element = document.at('link[@rel="canonical"]'))
+          Addressable::URI.parse(element['href'])
+        elsif (element = document.search('meta[@property$="url"]')
+                                 .select { |e| e['property'] == 'og:url' }
+                                 .first)
+          Addressable::URI.parse(element['content'])
+        else
+          response.uri
+        end
+      )
 
-      # The spec allows relative URIs in the "canonical" link
-      uri = Addressable::URI.join(response.uri.to_s, uri) unless uri.absolute?
+      # The spec allows the canonical link to be a relative URI
+      replace_self(Addressable::URI.join(response.uri.to_s, self)) unless absolute?
 
       # Because you can't always trust sites to do things properly
-      uri.normalize
+      normalize!
     end
 
     def fetch(limit = 50)
       Net::HTTP.start(hostname, port, use_ssl: scheme == 'https') do |http|
-        user_agent = 'Mozilla/5.0'
+        _response, uri = follow(http, self, limit, :head)
 
-        uri = dup
-        response = nil
-        limit.times do
-          request = Net::HTTP::Head.new(uri)
-          request['user-agent'] = user_agent
+        # Some (rude) sites don't allow HEAD requests
+        response, uri = follow(http, uri, (limit / 10).to_i, :get)
 
-          response = http.request(request)
-
-          break unless response.is_a?(Net::HTTPRedirection)
-
-          previous_uri = uri
-          uri = Addressable::URI.parse(response['location']).fixup
-
-          # Detect redirect loops early
-          break if uri == previous_uri
-        end
-
-        # Some (rude) sites don't allow HEAD requests, so check for redirects
-        (limit / 10).to_i.times do
-          request = Net::HTTP::Get.new(uri)
-          request['user-agent'] = user_agent
-
-          response = http.request(request)
-
-          break unless response.is_a?(Net::HTTPRedirection)
-
-          previous_uri = uri
-          uri = Addressable::URI.parse(response['location']).fixup
-
-          # Detect redirect loops early
-          break if uri == previous_uri
-        end
-
-        response.uri = uri if response.uri.nil?
+        response.uri ||= uri
         response
       end
     end
 
     def fixup
-      normalize
+      dup.fixup!
+    end
+
+    def fixup!
+      normalize!
+    end
+
+    private
+
+    def follow(http, uri, limit, method)
+      uri = uri.dup
+
+      limit.times do
+        request = case method
+                  when :get
+                    Net::HTTP::Get.new(uri)
+                  when :head
+                    Net::HTTP::Head.new(uri)
+                  end
+        request['user-agent'] = 'Mozilla/5.0'
+
+        response = http.request(request)
+
+        return response, uri unless response.is_a? Net::HTTPRedirection
+
+        previous_uri = uri
+        uri = Addressable::URI.parse(response['location'])
+        uri.fixup!
+
+        # Detect redirect loops early
+        return response, uri if uri == previous_uri
+      end
     end
   end
 end
--- lib/ruby/derpi.rb
+++ lib/ruby/derpi.rb
@@ -3,106 +3,94 @@ require 'json'
 
 module Addressable
   class URI
-    def fixup
-      uri = normalize
+    def fixup!
+      normalize!
 
       # Derpibooru
-      if uri.derpi?
-        match = uri.path.match(/\A\/(?:images\/)?(\d+)/)
-        return Addressable::URI.parse("https://derpibooru.org/#{match[1]}") if match
-        uri.hostname = 'derpibooru.org'
-      elsif uri.host.end_with?('derpicdn.net') && !uri.path.start_with?('/avatars/')
-        return Addressable::URI.parse(Addressable::URI.form_unencode(uri.query).to_h['url']).fixup if uri.host == 'camo.derpicdn.net'
-        match = uri.path.match(/\A.*\/(\d+)/)
-        return Addressable::URI.parse("https://derpibooru.org/#{match[1]}") if match
+      if derpi?
+        return replace_self(Addressable::URI.parse("https://derpibooru.org/#{id}")) if /\A\/(?:images\/)?(?<id>\d+)/ =~ @path
+        @host = 'derpibooru.org'
+      elsif @host == 'camo.derpicdn.net'
+        return replace_self(Addressable::URI.parse(Addressable::URI.form_unencode(@query).to_h['url']).fixup!)
+      elsif @host.end_with?('derpicdn.net') && !@path.start_with?('/avatars/')
+        return replace_self(Addressable::URI.parse("https://derpibooru.org/#{id}")) if /\A.*\/(?<id>\d+)/ =~ @path
 
       # desustorage
-      elsif uri.host.end_with?('desustorage.org')
-        uri.scheme = 'http' if uri.scheme == 'https'
+      elsif @host.end_with?('desustorage.org')
+        @scheme = 'http' if @scheme == 'https'
 
-      # deviantArt
-      elsif uri.host.end_with?('deviantart.com')
-        return Addressable::URI.parse(uri.query).fixup if uri.path == '/users/outgoing' && uri.query
-        if uri.fragment
-          match = uri.fragment.match(/\/(d\w{6})/)
-          return Addressable::URI.parse("http://fav.me/#{match[1]}") if match
-        end
-      elsif uri.host.end_with?('deviantart.net')
-        match = uri.path.split('/').last.match(/\A.*\b(d\w{6})/)
-        return Addressable::URI.parse("http://fav.me/#{match[1]}") if match
+      # DeviantArt
+      elsif @host.end_with?('deviantart.com')
+        return replace_self(Addressable::URI.parse(@query).fixup!) if @path == '/users/outgoing' && @query
+        return replace_self(Addressable::URI.parse("http://fav.me/#{id}")) if @fragment && /\/(?<id>d\w{6})/ =~ @fragment
+      elsif @host.end_with?('deviantart.net')
+        return replace_self(Addressable::URI.parse("http://fav.me/#{id}")) if /\A.*\b(?<id>d\w{6})\b/ =~ @path.split('/').last
 
       # e621
-      elsif uri.host.end_with?('e621.net')
-        if uri.host.start_with?('static')
-          match = uri.path.match(/\A\/data\/..\/..\/(\h{32})/)
-          if match
-            json = Addressable::URI.parse("https://e621.net/post/show.json?md5=#{match[1]}").fetch.body
-            return Addressable::URI.parse("https://e621.net/post/show/#{JSON.parse(json)['id']}") unless json.empty?
-          end
-        else
-          match = uri.path.match(/\A\/post\/show\/(\d+)/)
-          return Addressable::URI.parse("https://e621.net/post/show/#{match[1]}") if match
+      elsif @host.end_with?('e621.net')
+        if @host.start_with?('static') && /\A\/data\/..\/..\/(?<hash>\h{32})/ =~ @path
+          json = Addressable::URI.parse("https://e621.net/post/show.json?md5=#{hash}").fetch.body
+          return replace_self(Addressable::URI.parse("https://e621.net/post/show/#{JSON.parse(json)['id']}")) unless json.empty?
         end
+        return replace_self(Addressable::URI.parse("https://e621.net/post/show/#{id}")) if /\A\/post\/show\/(?<id>\d+)/ =~ @path
 
       # Imgur
-      elsif uri.host == 'i.imgur.com'
-        return Addressable::URI.parse("http://imgur.com/#{uri.path.split('/').last.split('.').first}")
+      elsif @host == 'i.imgur.com'
+        return replace_self(Addressable::URI.parse("http://imgur.com/#{@path.split('/').last.split('.').first}"))
 
       # Inkbunny
-      elsif uri.host.end_with?('inkbunny.net')
-        uri.fragment = nil if uri.fragment == 'pictop'
+      elsif @host.end_with?('inkbunny.net')
+        @fragment = nil if @fragment == 'pictop'
 
       # Know Your Meme
-      elsif uri.host.end_with?('kym-cdn.com')
-        return Addressable::URI.parse("http://knowyourmeme.com/photos/#{uri.path.split('/')[-4, 3].join('')}")
+      elsif @host.end_with?('kym-cdn.com')
+        return replace_self(Addressable::URI.parse("http://knowyourmeme.com/photos/#{@path.split('/')[-4, 3].join('')}"))
 
       # My Little Face When
-      elsif uri.host.end_with?('mylittlefacewhen.com')
-        match = uri.path.match(/\A\/media\/.*\/mlfw(\d+)/)
-        return Addressable::URI.parse("http://mylittlefacewhen.com/f/#{match[1]}") if match
+      elsif @host.end_with?('mylittlefacewhen.com')
+        return replace_self(Addressable::URI.parse("http://mylittlefacewhen.com/f/#{id}")) if /\A\/media\/.*\/mlfw(?<id>\d+)/ =~ @path
 
       # Pixiv
-      elsif uri.host.end_with?('pixiv.net')
-        uri.scheme = 'http' if uri.scheme == 'https'
+      elsif @host.end_with?('pixiv.net')
+        @scheme = 'http' if @scheme == 'https'
 
       # Rule 34
-      elsif uri.host.end_with?('rule34.paheal.net')
-        uri.scheme = 'http' if uri.scheme == 'https'
+      elsif @host.end_with?('rule34.paheal.net')
+        @scheme = 'http' if @scheme == 'https'
 
       # Tumblr
-      elsif uri.tumblr?
-        uri.fragment = nil if uri.fragment == '_=_'
-        if uri.path == '/login' && uri.query && uri.query_values['redirect_to']
-          uri.path = uri.query_values['redirect_to']
-          uri.query = nil
+      elsif tumblr?
+        @fragment = nil if @fragment == '_=_'
+        if @path == '/login' && @query && query_values['redirect_to']
+          @path = query_values['redirect_to']
+          @query = nil
         end
-        if uri.path.start_with?('/blog/')
-          return Addressable::URI.parse("#{uri.scheme}://#{uri.path.split('/')[2]}.tumblr.com/").fixup
-        end
-        match = uri.path.match(/\A\/\w+\/(\d+)/) unless uri.path.start_with?('/post/')
-        return Addressable::URI.parse("http://#{uri.host}/post/#{match[1]}") if match
-      elsif uri.host == 't.umblr.com'
-        return Addressable::URI.parse(Addressable::URI.form_unencode(uri.query).to_h['z']).fixup
+        return replace_self(Addressable::URI.parse("#{@scheme}://#{@path.split('/')[2]}.tumblr.com/").fixup!) if @path.start_with?('/blog/')
+        return replace_self(Addressable::URI.parse("http://#{@host}/post/#{id}")) if !@path.start_with?('/post/') && /\A\/\w+\/(?<id>\d+)/ =~ @path
+      elsif @host == 't.umblr.com'
+        return replace_self(Addressable::URI.parse(Addressable::URI.form_unencode(@query).to_h['z']).fixup!)
 
       end
 
-      uri
+      self
     end
 
+    private
+
     def derpi?
-      host.end_with?('derpibooru.org',
-                     'derpiboo.ru',
-                     'trixiebooru.org')
+      @host.end_with?('derpibooru.org',
+                      'derpiboo.ru',
+                      'trixiebooru.org')
     end
 
     def tumblr?
-      host.end_with?('tumblr.com',
-                     'fruitymilk.co.uk',
-                     'jayisbutts.com',
-                     'jykinturah.me',
-                     'kevinsano.com',
-                     'mangochan.com',
-                     'sugaryviolet.horse')
+      @host.end_with?('tumblr.com',
+                      'fruitymilk.co.uk',
+                      'jayisbutts.com',
+                      'jykinturah.me',
+                      'kevinsano.com',
+                      'mangochan.com',
+                      'sugaryviolet.horse')
     end
   end
 end
