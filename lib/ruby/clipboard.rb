b886fc3e7a0917fd60db1ebdb0e84430a7b625f1
David Wildasin <swdragonmaus@gmail.com>
2016-07-19 16:54:55 -0400

    Add documentation to clipboard.rb

--- lib/ruby/clipboard.rb
+++ lib/ruby/clipboard.rb
@@ -1,17 +1,29 @@
 require 'open3'
 
+# Public: Access the OS X clipboard.
 module Clipboard
   module_function
 
+  # Public: Clear the clipboard.
+  #
+  # Returns nothing.
   def clear
     copy('')
   end
 
+  # Public: Set the clipboard.
+  #
+  # data - The String data to copy into the clipboard.
+  #
+  # Returns the new contents of the clipboard as a String.
   def copy(data)
     Open3.popen3('pbcopy') { |i, _, _, _| i << data }
     paste
   end
 
+  # Public: Get the contents of the clipboard.
+  #
+  # Returns the contents of the clipboard as a String.
   def paste
     Open3.popen3('pbpaste') { |_, o, _, _| o.read }
   end
