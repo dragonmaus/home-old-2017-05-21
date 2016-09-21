b9259a18546a86c4b9ec6db9ff9acb2615f747b1
David Wildasin <swdragonmaus@gmail.com>
2016-09-20 20:04:57 -0400

    Bash `echo` no longer understands -n in sh emulation mode
    This is also not documented

--- .sh/functions/python.sh
+++ .sh/functions/python.sh
@@ -1,7 +1,7 @@
 a=`echo $PATH | tr : ' '`
 b=`find $a -name 'python*' ! -name 'python*-config' -print0 2>/dev/null | xargs -0 basename | sort -u`
 for c in $b; do
-  d=`echo -n $c | tr . _`
+  d=`echo $c | tr . _`
   test x$c = x$d || alias $c=$d
   eval $d'() (stty=`stty -g`; command '$c' ${1+"$@"}; r=$?; stty $stty; exit $r)'
 done
