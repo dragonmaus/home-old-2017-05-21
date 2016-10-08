af5c6c50b2803c76623c128b3d4a9089ca90ebee
David Wildasin <swdragonmaus@gmail.com>
2016-10-08 12:57:54 -0400

    Basic inspection for Ruby

---
+++ lib/ruby/dragon.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class Object
+  def diff_methods(other)
+    __send__(:methods).select { |s| s.to_s.end_with?('methods') && s != :diff_methods }.sort.map do |m|
+      if other.respond_to?(m)
+        [m, __send__(m).select { |s| !other.__send__(m).include?(s) }.sort]
+      else
+        [m, __send__(m).sort]
+      end
+    end.to_h
+  end
+end
