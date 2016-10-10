1e3898b912d85f78ebd5aaea4538dafb7bdb6cca
David Wildasin <swdragonmaus@gmail.com>
2016-10-10 11:25:24 -0400

    Update crunch to also remove excess blank lines at EOF

--- src/bin/crunch/crunch.sh
+++ src/bin/crunch/crunch.sh
@@ -7,7 +7,7 @@ usage() (die 100 usage 'crunch [bom | dos | eof | eol] path [expression]')
 case $1 in
 bom) filter() (iconv -f utf-8 -t utf-32 | iconv -f utf-32 -t utf-8);;
 dos) filter() (exec lefix);;
-eof) filter() (exec sed '');;
+eof) filter() (exec sed -e '$ {' -e '/^$/ d' -e '}');;
 eol) filter() (exec sed 's/[	 ]\{1,\}$//');;
 *) usage;;
 esac
