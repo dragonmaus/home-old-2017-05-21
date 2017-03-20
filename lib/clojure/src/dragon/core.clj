f42c14603ffdfd9cbef91c3a364aa80cceb22205
David Wildasin <swdragonmaus@gmail.com>
2017-03-20 15:17:26 -0400

    Because why not?

--- lib/clojure/src/dragon/core.clj
+++ lib/clojure/src/dragon/core.clj
@@ -1,5 +1,14 @@
 (ns dragon.core
-  (:refer-clojure))
+  (:refer-clojure)
+  (:require [clojure.string :as str]))
+
+(defn latin
+  "Onvertscay ethay inputay otay igpay atinlay."
+  [s]
+  (-> s
+      (str/lower-case)
+      (str/replace #"\b([bcdfghjklmnpqrstvwxz]*qu|y|[bcdfghjklmnpqrstvwxz]*)([aeiouy]\w*)\b" "$2$1ay")
+      (str/capitalize)))
 
 (defn ppmap
   "Partitioned parallel map.
--- lib/clojure/test/dragon/core_test.clj
+++ lib/clojure/test/dragon/core_test.clj
@@ -11,3 +11,16 @@
     (let [given "The quick brown fox jumps over the lazy dog."
           expected (apply str (map (comp char inc int) given))]
       (is (= expected (apply str (ppmap 5 (comp char inc int) given)))))))
+
+(deftest pig-latin
+  (testing "pig latin generator"
+    (testing "properly handles basic consonants"
+      (is (= "Atinlay" (latin "Latin"))))
+    (testing "properly handles y"
+      (is (= "Ouyay" (latin "You")))
+      (is (= "Ypetay" (latin "Type"))))
+    (testing "properly handles qu"
+      (is (= "Ickquay" (latin "Quick")))
+      (is (= "Eaksquay" (latin "Squeak"))))
+    (testing "properly handles full sentences with punctuation"
+      (is (= "Ethay ickquay ownbray oxfay umpsjay overay ethay azylay ogday." (latin "The quick brown fox jumps over the lazy dog."))))))
