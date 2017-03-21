f2b6bdcff168074f9a7f68c91917622553797968
David Wildasin <swdragonmaus@gmail.com>
2017-03-21 16:03:25 -0400

    Clean up profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,13 +1,9 @@
-{:repl {:dependencies [[dragon/libs "0.1.0-SNAPSHOT"]
-                       [environ "1.1.0"]]
-        :plugins [[lein-environ "1.1.0"]
-                  [lein-pprint "1.1.0"]]
+{:repl {:dependencies [[dragon/libs "0.1.0-SNAPSHOT"]]
+        :plugins [[lein-pprint "1.1.0"]]
         :repl-options {:init (do
-                               (require '[clojure.set :as set])
-                               (require '[clojure.string :as str])
-                               (require '[dragon.clipboard :as clipboard])
-                               (require '[dragon.core :refer :all])
-                               (require '[dragon.maths :as maths])
-                               (require '[environ.core :refer [env]])
-                               (intern 'environ.core 'env (into (sorted-map) environ.core/env)))}}
+                               (require '(clojure [set :as set]
+                                                  [string :as str])
+                                        '(dragon [clipboard :as clipboard]
+                                                 [core :refer :all]
+                                                 [maths :as maths])))}}
  :user {:plugins [[jonase/eastwood "0.2.3"]]}}
