fda33a9f35e787cff3417fdd1fc0d88dcbbcadc6
David Wildasin <swdragonmaus@gmail.com>
2017-03-24 23:48:28 -0400

    Windows CMD.EXE wrapper

---
+++ lib/clojure/src/dragon/cmd.clj
@@ -0,0 +1,52 @@
+(ns dragon.cmd
+  (:refer-clojure)
+  (:require (clojure [string :as str])
+            (clojure.java [shell :refer [sh]])))
+
+(defmulti ^:private cmd-fix*
+  (fn [data] (class data)))
+
+(defmethod cmd-fix* :default
+  [b] b)
+
+(defmethod cmd-fix* java.lang.String
+  [string]
+  (as-> string s
+    (str s \~)
+    (str/split-lines s)
+    (str/join \newline s)
+    (butlast s)
+    (str/join s)))
+
+(defn- cmd-fix
+  [{:keys [exit out err]}]
+  {:exit exit
+   :out (cmd-fix* out)
+   :err (cmd-fix* err)})
+
+(defn cmd!
+  [cmd & args]
+  (cmd-fix (apply sh "cmd.exe" "/c" cmd args)))
+
+(defn- print-maybe
+  [s]
+  (if (string? s)
+    (when (not (str/blank? s))
+      (print s))
+    (println s)))
+
+(defn- cmd-print
+  [{:keys [exit out err]}]
+  (print-maybe out)
+  (print-maybe err)
+  exit)
+
+(defn cmd
+  [cmd & args]
+  (cmd-print (apply cmd! cmd args)))
+
+(def ! cmd)
+(alter-meta! (var !) (fn [_] (meta (var cmd))))
+
+(def !! cmd!)
+(alter-meta! (var !!) (fn [_] (meta (var cmd!))))
