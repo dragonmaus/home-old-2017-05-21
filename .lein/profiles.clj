981b49f28ec3947cc6aebc4c2f875e76c1b78ba8
David Wildasin <swdragonmaus@gmail.com>
2017-04-05 09:04:27 -0400

    More fixes

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,23 @@
         :plugins [[lein-pprint "1.1.0"]]
         :repl-options {:init (do
                               (require '[clojure.set :as set]
-                                       '[clojure.string :as string]
+                                       '[clojure.string :as str]
+                                       'clojure.java.javadoc
                                        '[dragon.clipboard :as clipboard]
                                        '[dragon.core :refer :all]
-                                       '[dragon.maths :as maths]))}}
+                                       '[dragon.maths :as maths])
+                              (let [fix #(str/replace
+                                          %
+                                          #"http://java\.sun\.com/javase/7/"
+                                          "https://docs.oracle.com/javase/8/")]
+                                (intern 'clojure.java.javadoc
+                                        '*core-java-api*
+                                        (fix clojure.java.javadoc/*core-java-api*))
+                                (intern 'clojure.java.javadoc
+                                        '*remote-javadocs*
+                                        (ref (into (sorted-map)
+                                                   (map (fn
+                                                         [[k v]]
+                                                         [k (fix v)])
+                                                        @clojure.java.javadoc/*remote-javadocs*))))))}}
  :user {:plugins [[jonase/eastwood "0.2.3"]]}}
--- lib/clojure/build.boot
+++ lib/clojure/build.boot
@@ -1,12 +1,12 @@
 (set-env!
-  :resource-paths #{"src"}
-  :dependencies '[[adzerk/boot-test "1.2.0" :scope "test"]]
-  :source-paths #{"test"})
+ :resource-paths #{"src"}
+ :dependencies '[[adzerk/boot-test "1.2.0" :scope "test"]]
+ :source-paths #{"test"})
 
 (require '[adzerk.boot-test :refer :all])
 
 (task-options!
-  pom {:project 'dragon/libs
-       :version "0.1.0-SNAPSHOT"
-       :description "Miscellaneous personal code"}
-  aot {:all true})
+ pom {:project 'dragon/libs
+      :version "0.1.0-SNAPSHOT"
+      :description "Miscellaneous personal code"}
+ aot {:all true})
