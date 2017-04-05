d8c9ea6a80915118e894396e1a6933372990aa34
David Wildasin <swdragonmaus@gmail.com>
2017-04-05 15:50:09 -0400

    Clipboard rewrite

--- lib/clojure/src/dragon/clipboard.clj
+++ lib/clojure/src/dragon/clipboard.clj
@@ -1,15 +1,115 @@
 (ns dragon.clipboard
   (:refer-clojure)
   (:require [clojure.string :as str]
-            [dragon.core :refer [alias! getprop]]))
+            [clojure.java [io :as io]
+                          [shell :refer [sh]]]
+            [dragon.core :refer [getenv getprop]])
+  (:import [java.awt GraphicsEnvironment
+                     Toolkit]
+           [java.awt.datatransfer DataFlavor
+                                  StringSelection]))
 
-(case (first (str/split (getprop "os.name") #"\s+"))
-  "Windows" (require '[dragon.clipboard.awt :as internal])
-  "Mac"     (require '[dragon.clipboard.osx :as internal])
-  #_default (require '[dragon.clipboard.txt :as internal]))
+;;; Mode specification.
+(defn- has-pb?
+  []
+  (and (let [f (io/file "/usr/bin/pbcopy")]
+         (and (.exists f)
+              (.isFile f)
+              (.canExecute f)))
+       (let [f (io/file "/usr/bin/pbpaste")]
+         (and (.exists f)
+              (.isFile f)
+              (.canExecute f)))))
 
-(alias! copy internal/copy)
-(alias! paste internal/paste)
+(defn- has-gui?
+  []
+  (not (GraphicsEnvironment/isHeadless)))
+
+(def ^:dynamic *mode* nil)
+
+(defn- get-mode
+  []
+  (cond
+   (#{:pb :awt :file} *mode*) *mode*
+   (has-pb?)                  :pb
+   (has-gui?)                 :awt
+   :else                      :file))
+
+(defmacro with-mode
+  [mode & forms]
+  `(binding [*mode* ~mode]
+     ~@forms))
+
+;;; OSX implementation.
+(defn- pb-paste
+  []
+  (:out (sh "/usr/bin/pbpaste")))
+
+(defn- pb-copy
+  [s]
+  (sh "/usr/bin/pbcopy" :in (str s))
+  nil)
+
+;;; AWT implementation.
+(def ^:dynamic *awt-clipboard* (.getSystemClipboard (Toolkit/getDefaultToolkit)))
+
+(def ^:dynamic *awt-data-flavor* nil)
+
+(defmacro with-data-flavor
+  [flavor & forms]
+  `(let [df# (case ~flavor
+               :files DataFlavor/javaFileListFlavor
+               :html  DataFlavor/selectionHtmlFlavor
+               :image DataFlavor/imageFlavor
+                      DataFlavor/stringFlavor)]
+     (binding [*awt-data-flavor* df#
+               *mode* :awt]
+       ~@forms)))
+
+(defn- awt-paste
+  []
+  (let [df (or *awt-data-flavor*
+               ;(first (.getAvailableDataFlavors *awt-clipboard*))
+               DataFlavor/stringFlavor)]
+    (when (.isDataFlavorAvailable *awt-clipboard* df)
+      (.getData *awt-clipboard* df))))
+
+(defn- awt-copy
+  [s]
+  (as-> s s
+    (str s)
+    (StringSelection. s)
+    (.setContents *awt-clipboard* s nil)))
+
+;;; File implementation.
+(def ^:dynamic *file-clipboard*
+  (let [file (getenv "CLIPBOARD")
+        file (or (and file (io/file file))
+                 (io/file (getprop "user.home") ".clipboard"))]
+    (doto file (.createNewFile))))
+
+(defn- file-paste
+  []
+  (slurp *file-clipboard*))
+
+(defn- file-copy
+  [s]
+  (spit *file-clipboard* (str s)))
+
+;;; Core functions.
+(defn paste
+  []
+  (case (get-mode)
+    :pb   (pb-paste)
+    :awt  (awt-paste)
+    :file (file-paste)))
+
+(defn copy
+  [s]
+  (case (get-mode)
+    :pb   (pb-copy s)
+    :awt  (awt-copy s)
+    :file (file-copy s)))
 
 (defn clear
   []
