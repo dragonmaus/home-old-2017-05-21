442e2ad6789cd6860e13ab2694ae0877595d2928
David Wildasin <swdragonmaus@gmail.com>
2017-04-04 21:24:53 -0400

    Update lein config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:repl {:dependencies [[dragon/libs "0.1.0-SNAPSHOT"]]
+{:repl {:dependencies [[dragon/libs "1.0.0-SNAPSHOT"]]
         :plugins [[lein-pprint "1.1.0"]]
         :repl-options {:init (do
-                               (require '(clojure [set :as set]
-                                                  [string :as str])
-                                        '(dragon [clipboard :as clipboard]
-                                                 [core :refer :all]
-                                                 [maths :as maths])))}}
+                              (require '[clojure.set :as set]
+                                       '[clojure.string :as string]
+                                       '[dragon.clipboard :as clipboard]
+                                       '[dragon.core :refer :all]
+                                       '[dragon.maths :as maths]))}}
  :user {:plugins [[jonase/eastwood "0.2.3"]]}}
