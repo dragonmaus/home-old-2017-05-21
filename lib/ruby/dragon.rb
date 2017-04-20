98f17067f3bc2ff358351debe3979648cfe17ea3
David Wildasin <swdragonmaus@gmail.com>
2017-04-19 20:55:32 -0400

    Rubocop

--- lib/ruby/dragon.rb
+++ lib/ruby/dragon.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 class Object
   def diff_methods(other)
     __send__(:methods).select { |s| s.to_s.end_with?('methods') && s != :diff_methods }.sort.map do |m|
       if other.respond_to?(m)
-        [m, __send__(m).select { |s| !other.__send__(m).include?(s) }.sort]
+        [m, __send__(m).reject { |s| other.__send__(m).include?(s) }.sort]
       else
         [m, __send__(m).sort]
       end
